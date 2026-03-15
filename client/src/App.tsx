645dc7aec8310698db4bcac671d8508e5bbf6e23
jcheigh <justincheigh@gmail.com>
2026-03-15 17:00:52 -0400

    vite server fetches notes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,34 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <>
-      <section id="center">
-        <p> Hello, World! </p>
-      </section>
-    </>
-  )
+type Note = {
+  title: string;
+  createdAt: string;
+  body: string;
 }
 
-export default App
+export default function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+  
+  useEffect(() => {
+    async function loadNotes() {
+      const response = await fetch("/api/notes");
+      const data = await response.json();
+      setNotes(data);
+    }
+
+    loadNotes();
+  }, []);
+
+  return (
+    <div>
+      {notes.map((note) => (
+        <div key={`${note.title}-${note.createdAt}`}>
+          <h3>{note.title}</h3>
+          <div>{note.createdAt}</div>
+          <p>{note.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
